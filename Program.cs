35d4ee7efc485a96c30186455ffe7c95730513ae
Beratdmrbs <beratdmrbs61@gmail.com>
2024-10-20 21:46:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double sayi1 = 0, sayi2 = 0;
+Console.Write("Birinci sayıyı giriniz:");
+sayi1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("İkinci sayıyı giriniz:");
+sayi2 = Convert.ToDouble(Console.ReadLine());
+
+if (sayi1 >= sayi2)
+    Console.WriteLine("FARK={0}", sayi1 - sayi2);
+else
+    Console.WriteLine("FARK={0}", sayi2 - sayi1);
+
+Console.ReadKey();
